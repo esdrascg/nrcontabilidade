7890255c3629807a2b48d37ea652527563ce632e
esdrascg <esdrascgama@gmail.com>
2021-02-25 22:53:08 -0300

    --branch

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      */
 
     protected $fillable = [
-        'id','name', 'email', 'password',
+        'id','name', 'email', 'password',  'telefone',
     ];
 
     /**
