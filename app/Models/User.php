97cf94eff3f437bdbd3004cf87cb83efe6937ac9
esdrascg <esdrascgama@gmail.com>
2021-02-25 23:03:21 -0300

    retirar campo

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      */
 
     protected $fillable = [
-        'id','name', 'email', 'password',  'phone',
+        'id','name', 'email', 'password',
     ];
 
     /**
