0c5195b708ffbcf717db212e8a9e0e58f733185c
esdrascg <esdrascgama@gmail.com>
2021-02-25 22:59:14 -0300

    acrescentar campo em user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      */
 
     protected $fillable = [
-        'id','name', 'email', 'password',  'telefone',
+        'id','name', 'email', 'password',  'phone',
     ];
 
     /**
